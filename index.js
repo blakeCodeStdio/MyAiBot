dfbffeffbdf7c9f2c0246df31005b127ea6023ab
blakeCodeStdio <hiblake1999@gmail.com>
2025-02-04 14:46:01 +0800

    更改模型版本

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on('message', async (event) => {
     const response = await axios.post(
       'https://api.groq.com/openai/v1/chat/completions',
       {
-        model: 'mixtral-8x7b-32768',
+        model: 'llama3-70b-8192',
         messages: [{ role: 'user', content: userMessage }],
       },
       {
